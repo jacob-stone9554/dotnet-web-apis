f6fccb16b843379a2ce419c9bc28e3641d785a48
Jacob Stone <jumboshrimp9554@gmail.com>
2024-05-14 16:52:44 -0700

    Documentation update

--- VGList/Program.cs
+++ VGList/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,12 +13,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapGet("/error", () => Results.Problem());//map errors to this Minimal API controller.
+app.MapGet("/error/test", () => { throw new Exception("test"); }); //this should take you to an error page with the exception message of "test"
 app.MapControllers();
 
 app.Run();
